0809645ea339620b3ab3e9778759974d5db97f27
Jared Ross <JaredARoss14@gmail.com>
2017-12-13 15:19:26 -0500

    added basic links

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC-p5vXvySUglv6cUPtY8p9SLgU54MLCzc",
+    authDomain: "train-schedule-c9279.firebaseapp.com",
+    databaseURL: "https://train-schedule-c9279.firebaseio.com",
+    projectId: "train-schedule-c9279",
+    storageBucket: "",
+    messagingSenderId: "830054784394"
+  };
+  firebase.initializeApp(config);
+</script>
